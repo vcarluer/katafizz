ab66bc09d2487a2672ef40d34e0426649b14b680
Vincent Carluer <vcarluer@gmail.com>
2016-03-15 21:55:23 +0000

    all tests

--- src/fizz.js
+++ src/fizz.js
@@ -1,7 +1,19 @@
 module.exports = { 
-    getOutput: function(number){return 'fizz'}
-    
-    
-    
-    
+    getOutput: function(number){
+        var fizzbuzz = ''
+        
+        if(number%3 === 0 || number.toString().indexOf('3')> -1){
+            fizzbuzz = 'fizz'
+        }
+        
+        if(number%5 === 0|| number.toString().indexOf('5')> -1){
+            fizzbuzz += 'buzz'
+        }
+        
+        if(fizzbuzz =='')
+        {
+            fizzbuzz = number.toString()
+        }
+        return fizzbuzz
+    }
 }
--- test/test.js
+++ test/test.js
@@ -7,4 +7,45 @@ describe('fizztest', ()=>{
         var output = fizz.getOutput(3)
         expect(output).to.equal('fizz')
     })
+    
+    it('should return fizz if numer is 6', ()=>{
+        var output = fizz.getOutput(6)
+        expect(output).to.equal('fizz')
+    })
+    
+    it('should return buzz if number is 5', ()=>{
+        var output = fizz.getOutput(5)
+        expect(output).to.equal('buzz')
+    })
+    
+    it('should return buzz if numer is 10', ()=>{
+        var output = fizz.getOutput(10)
+        expect(output).to.equal('buzz')
+    })
+    
+    it('should return buzz if number is 88', ()=>{
+        var output = fizz.getOutput(88)
+        expect(output).to.equal('88')
+    })
+    
+    it('should return fizzbuzz if number is 15', ()=>{
+        var output = fizz.getOutput(15)
+        expect(output).to.equal('fizzbuzz')
+    })
+    
+    it('should return fizz if number is 83', ()=>{
+        var output = fizz.getOutput(83)
+        expect(output).to.equal('fizz')
+    })
+    
+    it('should return buzz if number is 59', ()=>{
+        var output = fizz.getOutput(59)
+        expect(output).to.equal('buzz')
+    })
+    
+    it('should return fizz if number is 53', ()=>{
+        var output = fizz.getOutput(53)
+        expect(output).to.equal('fizzbuzz')
+    })
+    
 })
